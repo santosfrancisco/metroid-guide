fb41e1ede5afafaa918e167768106fae4f15886d
Francisco Santos <fwfs1985@gmail.com>
2020-02-24 14:30:37 -0300

    headings component

---
+++ src/components/headings/headings.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  font-size: 2em;
+  margin-bottom: 16px;
+`
+
+const H2 = styled.h2`
+  font-size: 1.5em;
+  margin-bottom: 16px;
+`
+
+export {
+  H1,
+  H2,
+};
