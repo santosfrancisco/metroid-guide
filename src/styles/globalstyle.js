e13e44d1cd0e90178e9eef02ea6925f7718b84de
Francisco Santos <fwfs1985@gmail.com>
2020-02-24 02:17:09 -0300

    first commit

---
+++ src/styles/globalstyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif ;
+    background-color: #000;
+    color: #fff;
+  }
+`
